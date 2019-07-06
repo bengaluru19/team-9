ad3f07dff53bbc019d93c2873cce3f11c633abce
Amogh13246831 <42728157+Amogh13246831@users.noreply.github.com>
2019-07-07 00:38:25 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 var express = require("express");
 var bp = require("body-parser");
-var firebase = require('firebase');
-
-var config = {
-    apiKey: "",
-    authDomain: "careworks-b683b.firebaseapp.com",
-    databaseURL: "https://careworks-b683b.firebaseio.com",
-    projectId: "careworks-b683b",
-    storageBucket: "careworks-b683b.appspot.com",
-    messagingSenderId: "575148253636",
-    appId: "1:575148253636:web:4e2d2181cb257653"
-};
-firebase.initializeApp(config);
 
 var app = express();
 app.use(bp.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public'));
 
 var mysql = require('mysql');
-
+/*
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -29,29 +17,10 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
 });
-
+*/
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/reg.html");
 });
-
-app.get('/fetch', function (req, res) {
-
-	console.log("HTTP Get Request");
-	var userReference = firebase.database().ref("/volunteers/");
-
-	//Attach an asynchronous callback to read the data
-	userReference.on("value", 
-			  function(snapshot) {
-					console.log(snapshot.val());
-					res.json(snapshot.val());
-					userReference.off("value");
-					}, 
-			  function (errorObject) {
-					console.log("The read failed: " + errorObject.code);
-					res.send("The read failed: " + errorObject.code);
-			 });
-});
-
 
 var volunteers = 0
 app.post("/formdata", (req, res) => {
@@ -66,24 +35,18 @@ app.post("/formdata", (req, res) => {
         {name:"teach english", city:"chennai", date:"43/22/19910", numPeople:"80"}];
     res.render("about.ejs", {events: res.events});
     // write db code here
-    var refpath = '/volunteers/' + inp.id + '/';
-    console.log(inp);
-    firebase.database().ref(refpath)
-    .set(inp ,(err) => {
-         if(err)
-         console.log(err)
-    });
 });
 
 
 app.get("/admin", (req, res) => {
-    var volunteers;
-
+    var volunteers = [{name:"A", email:"B", phone:"C", events:6, hours:55}];
+    var events = [{name:"FP", city:"Bengaluru", date:"27/07/27", activity:"Finger painting", numPeople: 80}];
+/*
     con.query("SELECT * FROM events", function (err, result, fields) {
         if (err) throw err;
         volunteers = JSON.parse(JSON.stringify(result));
-    });
-    var data = {numVols: 0, numEvents: 0, numSchools: 0, volunteers: volunteers};
+    });*/
+    var data = {numVols: 0, numEvents: 0, numSchools: 0, volunteers: volunteers, events: events};
     res.render("admin.ejs", data);
 });
 
