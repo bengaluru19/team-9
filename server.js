af91d6888146bb8043b203f8506965890cc59664
Amogh13246831 <42728157+Amogh13246831@users.noreply.github.com>
2019-07-06 19:55:09 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var bp = require("body-parser");
 
 var app = express();
 app.use(bp.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/reg.html");
@@ -14,7 +16,9 @@ app.post("/formdata", (req, res) => {
     inp.email = req.body.email;
     inp.phone = req.body.phone;
     console.log(inp);
-
+    res.events = [{name:"paint bottles", city:"bengaluru", date:"07/07/2177", numPeople:"560"},
+        {name:"teach english", city:"chennai", date:"43/22/19910", numPeople:"80"}];
+    res.render("about.ejs", {events: res.events});
     // write db code here
 });
 
